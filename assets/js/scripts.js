15516bd79f4f0c7214776fd5691d884499b43674
Thomas <tahjones75@gmail.com>
2020-06-11 10:47:40 +0000

    removed commented out code

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -6,19 +6,11 @@ function responsiveTitle(pageWidth) {
     if(pageWidth.matches) {
         title.textContent = "Thomas Jones BSc LCHE - Classical Homeopath";
         bannerImg.src = "assets/images/homeopathy-montage--large.png";
-        // bannerImg.src = "assets/images/homeopathy_montage_290520-5.png";
     } else {
         title.textContent = "Thomas Jones - Homeopath";
         bannerImg.src = "assets/images/homeopathy-montage--small.png";
     }
 }
 
-// document.addEventListener('DOMcontentloaded', function(){
-// document.addEventListener('resize', function(){
-    responsiveTitle(xs);
-    xs.addListener(responsiveTitle);
-// }, false)
-
-// document.addEventListener('resize', function() {
-//     responsiveTitle(xs, title);
-// }, false);
+responsiveTitle(xs);
+xs.addListener(responsiveTitle);
