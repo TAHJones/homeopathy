59dd0283509fab510bc9bfd4d1697b6b19fae6e4
Thomas <tahjones75@gmail.com>
2020-06-21 19:23:31 +0000

    added script for bootstrap form custom validation

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -141,6 +141,27 @@ function responsiveCards() {
 }
 
 
+// function for bootstrap custom form validation taken from https://www.w3schools.com/bootstrap4/bootstrap_forms.asp
+// Disable form submissions if there are invalid fields
+(function() {
+  'use strict';
+  window.addEventListener('load', function() {
+    // Get the forms we want to add validation styles to
+    let forms = document.getElementsByClassName('needs-validation');
+    // Loop over them and prevent submission
+    let validation = Array.prototype.filter.call(forms, function(form) {
+      form.addEventListener('submit', function(event) {
+        if (form.checkValidity() === false) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+        form.classList.add('was-validated');
+      }, false);
+    });
+  }, false);
+})();
+
+
 // eventlisteners
 sm.addListener(responsiveTitle);
 sm.addListener(getCurrentYear);
